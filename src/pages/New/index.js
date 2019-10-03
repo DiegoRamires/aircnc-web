4568a063642c40a122241771aefcaaa6f60011d4
DiegoRamires <goncalves.ramires@gmail.com>
2019-10-03 20:30:40 -0300

    add navigation button to /new route. Styling form on screen

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // import { Container } from './styles';
 
 export default function New() {
+  const [company, setCompany] = useState('')
+  const [techs, setTechs] = useState('')
+  const [price, setPrice] = useState('')
+
+  function handleSubmit() {
+
+  }
+
   return (
-    <div />
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="company">EMPRESA *</label>
+      <input
+        id="company"
+        placeholder="Sua empresa incrível"
+        value={company}
+        onChange={event => setCompany(event.target.value)}
+      />
+
+      <label htmlFor="techs">
+        TECNOLOGIAS * <span>(separadas por vírgula)</span>
+      </label>
+      <input
+        id="techs"
+        placeholder="Quais tecnologias usam?"
+        value={techs}
+        onChange={event => setTechs(event.target.value)}
+      />
+
+      <label htmlFor="price">
+        VALOR DA DIÁRIA * <span>(em branco para GRATUITO)</span>
+      </label>
+      <input
+        id="price"
+        placeholder="Valor cobrado por dia"
+        value={price}
+        onChange={event => setPrice(event.target.value)}
+      />
+
+      <button type="submit" className="btn">Cadastrar</button>
+    </form>
   );
 }
